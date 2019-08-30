74ef01902f44fee6101122550ee26158033de924
Panchanok <panchanok@panchanoks-air.home>
2019-08-30 19:39:41 -0400

    add padding

--- sound.py
+++ sound.py
@@ -2,64 +2,123 @@
 import librosa
 import numpy as np
 import os
+import re
+import csv
 
+global hop_length
+
+
+# Set the hop length; at 22050 Hz, 512 samples ~= 23ms
+hop_length = 128
+
+##save the current directory
+cwd = os.getcwd()
+
+##change to sound file directory !!hard-coded
 audio_dir = '/Users/panchanok/Desktop/PyHack2019/sound/tone_perfect/'
-audio_files = os.listdir(audio_dir)
+os.chdir(audio_dir)
 
-#audio_path = '/Users/panchanok/Desktop/PyHack2019/sound/tone_perfect/a1_FV1_MP3.mp3'
+##list files in the directory
+audio_files = os.listdir(audio_dir)[1:10]
+#print(audio_files)
 
-## return a one-D array with mcff, mcff_delta, and chomagram of the audio file
-def vectorize(audio_path):
 
+## return a (flatten) one-D array of mfcc of an audio file
+def getMFCC(audio_file):
 
-    y, sr = librosa.load(audio_path)
+    print('*getting ', audio_file)
+    y, sr = librosa.load(audio_file)
+    # Compute MFCC features from the raw signal
+    return librosa.feature.mfcc(y=y, sr=sr, hop_length=hop_length, n_mfcc=13).flatten()
 
-    # Set the hop length; at 22050 Hz, 512 samples ~= 23ms
-    hop_length = 32
+## return a (flatten) one-D array of chromagram of an audio file
+def getChroma(audio_file):
 
+    y, sr = librosa.load(audio_file)
     # Separate harmonics and percussives into two waveforms
     y_harmonic, y_percussive = librosa.effects.hpss(y)
 
-    # Beat track on the percussive signal
-    tempo, beat_frames = librosa.beat.beat_track(y=y_percussive,
-                                                 sr=sr)
-
-    # Compute MFCC features from the raw signal
-    mfcc = librosa.feature.mfcc(y=y, sr=sr, hop_length=hop_length, n_mfcc=13)
-
-    # And the first-order differences (delta features)
-    mfcc_delta = librosa.feature.delta(mfcc)
-
-    # # Stack and synchronize between beat events
-    # # This time, we'll use the mean value (default) instead of median
-    # beat_mfcc_delta = librosa.util.sync(np.vstack([mfcc, mfcc_delta]),
-    #                                     beat_frames)
-
     # Compute chroma features from the harmonic signal
-    chromagram = librosa.feature.chroma_cqt(y=y_harmonic,
-                                            sr=sr)
-
-    # # Aggregate chroma features between beat events
-    # # We'll use the median value of each feature between beat frames
-    # beat_chroma = librosa.util.sync(chromagram,
-    #                                 beat_frames,
-    #                                 aggregate=np.median)
-
-    # # Finally, stack all beat-synchronous features together
-    # beat_features = np.vstack([beat_chroma, beat_mfcc_delta])
-    # #tempo, neat_frames = librosa.beat.beat_track(y=y, sr=sr)
-
-    #processed_input = np.vstack([beat_features, beat_chroma, beat_mfcc_delta])
+    return librosa.feature.chroma_cqt(y=y_harmonic,
+                                            sr=sr).flatten()
 
 
-    return np.hstack([mfcc.flatten(), mfcc_delta.flatten(), chromagram.flatten()])
+## return a 2-d array of MFCC padded with 0's of ALL audio files
+def getPaddedMFCC(audio_files):
+    result = [getMFCC(f) for f in audio_files]
 
-### vecterize all audio files
-input = np.array([vectorize(audio_dir + f) for f in audio_files[1:5]])
-print(input[1][4])
-### Export list of arrays
-with open('______ss.csv', 'w') as f:
-    for item in input:
-        f.write("%s\n" % item)
+    ##pad arrays with 0's. Get arrays of size Max
+    max_len = max([len(x) for x in result])
+    padded = [np.pad(x, (0, max_len - len(x))) for x in result]
 
-print('succeed')
+    ##sanity check
+    is_shorter = sum([len(x) - max_len for x in padded])
+    if is_shorter < 0:
+        print('not padded well')
+        return -1
+    else:
+        return padded
+
+## return a 2-d array of chromagram padded with 0's of ALL audio files
+def getPaddedChroma(audio_files):
+    result = [getChroma(f) for f in audio_files]
+
+    ##pad arrays with 0's. Get arrays of size Max
+    max_len = max([len(x) for x in result])
+    padded = [np.pad(x, (0, max_len - len(x))) for x in result]
+
+    ##sanity check
+    is_shorter = sum([len(x) - max_len for x in padded])
+    if is_shorter < 0:
+        print('not padded well')
+        return -1
+    else:
+        return padded
+
+print('getting mfcc')
+mfcc = getPaddedMFCC(audio_files)
+print('getting chromagram')
+chrom = getPaddedChroma(audio_files)
+
+if mfcc == -1 or chrom == -1:
+    print('ATTENTION: some instance is not padded')
+    exit()
+
+## concatenate mfcc and chrom features
+att_input = [np.hstack([m, c]) for m, c in zip(mfcc, chrom)]
+
+## checking the final length
+#print(len(mfcc[5]), len(chrom[5]), len(x[5]))
+
+
+##detect targets from sound names
+p = re.compile('^[aeoU]|[bcdfghjklmnpqrstwxyz]+(?=[aeiou])')
+target = [p.match(f).group() for f in audio_files]
+
+##for debugging
+# for i in range(len(audio_files)):
+#     f = audio_files[i]
+#     print(f)
+#     p.match(f).group()
+
+## check corrrectness
+#print(target)
+#print(audio_files)
+
+
+## tag labels to attributes
+labeled_input = np.array([np.hstack([i, l]) for i, l in zip(att_input, target)])
+
+
+from datetime import datetime
+
+
+os.chdir(cwd)
+export_name = 'processed_audio_' + datetime.now().strftime('%H_%M_%S') + '.csv'
+with open(export_name,"w+") as processed:
+    csvWriter = csv.writer(processed,delimiter=',')
+    csvWriter.writerows(labeled_input)
+
+## check final lengths
+#print(len(att_input[3]), len(labeled_input[3]), labeled_input)
+print('processed input is saved at ', export_name)
